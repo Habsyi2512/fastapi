06f177fdc9302908c1e6cb7b2336f768b270a7ac
Habsyi2512 <githabsyi@gmail.com>
2025-01-27 22:22:03 +0700

    Remove unused API endpoint functions from main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -15,19 +15,3 @@ app.add_middleware(
   allow_methods=["*"],
   allow_headers=["*"]
 )
-
-@app.get("/api")
-def read_root():
-  return {"Hello":"World"}
-
-@app.get("/api/items/{id}")
-def read_item(id:int, q: Union[int, None] = None):
-  return {"item_id":id, "q":q}
-
-@app.get("/api/get-profile")
-def get_profile():
-  return {
-    "nama":"Muhammad Habsyi Mubarak",
-    "umur":21,
-    "pekerjaan":"Mahasiswa",
-  }
