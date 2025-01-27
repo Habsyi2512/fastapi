875d4dcf9660af48bbdb2167cf4711c873a03ba9
Habsyi2512 <githabsyi@gmail.com>
2025-01-27 20:57:47 +0700

    Refactor homepage to fetch and display user profile data

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,15 +1,25 @@
-async function getData() {
-  const response = await fetch("http://localhost:8000/api");
+type Profile = {
+  nama: string;
+  umur: number;
+  pekerjaan: string;
+};
+
+async function getProfile(): Promise<Profile> {
+  const response = await fetch("http://localhost:8000/api/get-profile");
   const data = await response.json();
   return data;
 }
 
 export default async function Home() {
-  const data = await getData();
+  const dataProfile = await getProfile();
   return (
-    <div>
-      <h1>Halaman Utama</h1>
-      <p>{data.Hello}</p>
+    <div className="w-full">
+      <h1 className="text-2xl text-center py-8 text-neutral-700 font-bold">Halaman Utama</h1>
+      <div className="w-full max-w-xl p-2 shadow rounded mx-auto bg-neutral-300">
+        <p>nama : {dataProfile.nama}</p>
+        <p>umur : {dataProfile.umur}</p>
+        <p>pekerjaan : {dataProfile.pekerjaan}</p>
+      </div>
     </div>
   );
 }
